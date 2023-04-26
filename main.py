83e72db2ef99c17c8db0d2f0354b02646dfab432
NIcKnoMask <zhouxuanhao64@gmail.com>
2023-04-26 18:46:55 +1000

    modify the prediction process

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from GateCNN import GateCNN
 from TextCNN import TextCNN
 from RNN_LSTM import LSTM
 import pandas as pd
+import numpy as np
 from metrics import train, validate, predict, evaluation
 
 
@@ -12,10 +13,6 @@ train_data = CustomDataset(1)
 validate_data = CustomDataset(domain=1, train=False)
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
 validate_loader = torch.utils.data.DataLoader(validate_data, batch_size=32, shuffle=False)
-
-# The data that used to predict toe final result in kaggle
-test_data = TestSet()
-test_loader = torch.utils.data.DataLoader(test_data, batch_size=50, shuffle=False)
 
 # initial classifier
 TextCNNclf = TextCNN()
@@ -27,10 +24,20 @@ optimizer = torch.optim.SGD(TextCNNclf.parameters(), lr=1e-2, momentum=0.9)
 conv_loss, conv_acc = train(TextCNNclf, train_loader, validate_loader, optimizer)
 
 # make prediction
-pred = predict(TextCNNclf, test_loader)
+test_data1 = TestSet(domain=1)
+test_data2 = TestSet(domain=2)
+test_loader1 = torch.utils.data.DataLoader(test_data1, batch_size=25, shuffle=False)
+test_loader2 = torch.utils.data.DataLoader(test_data2, batch_size=25, shuffle=False)
 
-# output the prediction as .csv
-pred = pd.Series(pred)
+# train the model for domain1
+pred_domain1 = predict(TextCNNclf, test_loader1)
+pred_domain1 = pred_domain1.numpy()
+
+# use the pre-trained model to fit domain2
+pred_domain2 = predict(TextCNNclf, test_loader2)
+pred_domain2 = pred_domain2.numpy()
+
+pred = np.concatenate((pred_domain1, pred_domain2), axis=None)
 d = {'Id': [i for i in range(len(pred))], 'Predicted': pred}
 output = pd.DataFrame(d)
 output.to_csv('result.csv', index=False)
